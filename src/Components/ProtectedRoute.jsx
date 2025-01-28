82c4a53e3bd0400373b968a2fae5b9b5bbce60c7
tanishktiwari <155141073+tanishktiwari@users.noreply.github.com>
2025-01-28 14:01:14 +0530

    Update ProtectedRoute.jsx

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, isAdminRoute }) => {
   const location = useLocation();
-  const isAdminLoggedIn = localStorage.getItem('adminLoggedIn'); // Check if the admin is logged in
 
-  if (!isAdminLoggedIn) {
-    // If not logged in as admin, redirect to the admin login page
+  // Retrieve login statuses
+  const isUserLoggedIn = localStorage.getItem('loggedInUserMobileNumber');
+  const isAdminLoggedIn = localStorage.getItem('adminLoggedIn');
+
+  // Check for admin routes
+  if (isAdminRoute && !isAdminLoggedIn) {
     return <Navigate to="/admin-login" state={{ from: location }} replace />;
+  }
+
+  // Check for user routes
+  if (!isAdminRoute && !isUserLoggedIn) {
+    return <Navigate to="/" state={{ from: location }} replace />;
   }
 
   return children;

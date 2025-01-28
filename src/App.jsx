ebec0d59af230408258c8cc6da8d72b8ee69fa07
tanishktiwari <155141073+tanishktiwari@users.noreply.github.com>
2025-01-28 12:18:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,35 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 // Import ProtectedRoute
 import ProtectedRoute from './Components/ProtectedRoute';
 
-// Import UserPanel, AdminPanel, and other components
+// Import other components
 import UserPanel from './Components/Loginpages/Userpanel';
 import AdminPanel from './Components/Loginpages/Adminpanel';
 import Dashboard from './Components/UserPanel/Dashboard/Dashboard';
 import CreateTicket from './Components/UserPanel/Createticket/Createticket';
 import Open from './Components/UserPanel/Open/Open';
 import Close from './Components/UserPanel/Close/Close';
-import Dashboardadmin from './Components/AdminPanel/Dashboardadmin';
-import Operator from "./Components/AdminPanel/Operator/Operator";
 import Home from './Components/UserPanel/Home/Home';
 import ProfileForm from './Components/UserPanel/Profileform/Profileform';
-import OpenticketAdmin from './Components/AdminPanel/OpenticketAdmin/OpenticketAdmin';
-import CloseticketAdmin from './Components/AdminPanel/CloseticketAdmin/CloseticketAdmin';
 import Reports from './Components/UserPanel/Reports/Reports';
 import PPMStatus from './Components/UserPanel/PPM/PPMStatus';
 import MonthlyHealthCheck from './Components/UserPanel/PPM/MonthlyHealthCheck';
 import InventoryInfo from './Components/UserPanel/PPM/InventoryInfo';
+import AddEmailsPage from './Components/UserPanel/Dashboard/Navbar/AddEmailsPage';
+import PrivacyPolicyPage from './Components/Footer/PrivacyPolicyPage';
+import TermsOfService from './Components/Footer/TermsOfService';
+import SecurityPolicyPage from './Components/Footer/SecurityPolicyPage';
+
+import Dashboardadmin from './Components/AdminPanel/Dashboardadmin';
+import Operator from './Components/AdminPanel/Operator/Operator';
+import OpenticketAdmin from './Components/AdminPanel/OpenticketAdmin/OpenticketAdmin';
+import CloseticketAdmin from './Components/AdminPanel/CloseticketAdmin/CloseticketAdmin';
 import IssueCategoryDetails from './Components/AdminPanel/IssueCategory/IssueCategoryDetails';
 import Engineers from './Components/AdminPanel/Engineers/Engineers';
 import Company from './Components/AdminPanel/Company/Company';
-import AddEmailsPage from './Components/UserPanel/Dashboard/Navbar/AddEmailsPage';
 import PpmForm from './Components/AdminPanel/Company/PpmForm';
 import HealthCheckForm from './Components/AdminPanel/Company/HealthCheckForm';
 import PeriodicReport from './Components/AdminPanel/Sidebar/PeriodicReport';
 import PromptManager from './Components/AdminPanel/Sidebar/PromptManager';
-import PrivacyPolicyPage from './Components/Footer/PrivacyPolicyPage';
-import TermsOfService from './Components/Footer/TermsOfService';
-import SecurityPolicyPage from './Components/Footer/SecurityPolicyPage';
 
 function App() {
   return (
@@ -45,34 +46,33 @@ function App() {
                 <Route path="/" element={<UserPanel />} />
                 <Route path="/admin-login" element={<AdminPanel />} />
 
-                {/* Protected Dashboard Routes with Wildcard */}
-                <Route 
-                  path="/dashboard/*" 
+                {/* Protected User Dashboard Routes with Wildcard */}
+                <Route
+                  path="/dashboard/*"
                   element={
                     <ProtectedRoute>
                       <Dashboard />
                     </ProtectedRoute>
                   }
                 >
-                  {/* Nested Dashboard Routes */}
                   <Route path="create-ticket" element={<CreateTicket />} />
                   <Route path="open" element={<Open />} />
                   <Route path="close" element={<Close />} />
                   <Route path="home" element={<Home />} />
-                  <Route path="Profileform" element={<ProfileForm />} />
+                  <Route path="profileform" element={<ProfileForm />} />
                   <Route path="report" element={<Reports />} />
                   <Route path="ppm-status" element={<PPMStatus />} />
                   <Route path="monthly-healthcheck" element={<MonthlyHealthCheck />} />
                   <Route path="spare-inventory" element={<InventoryInfo />} />
                   <Route path="add-emails" element={<AddEmailsPage />} />
-                  <Route path='PrivacyPolicyPage' element={<PrivacyPolicyPage />} />
-                  <Route path='TermsOfService' element={<TermsOfService />} />
-                  <Route path='security-policy' element={<SecurityPolicyPage />} />
+                  <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
+                  <Route path="terms-of-service" element={<TermsOfService />} />
+                  <Route path="security-policy" element={<SecurityPolicyPage />} />
                 </Route>
 
                 {/* Protected Admin Dashboard Routes */}
-                <Route 
-                  path="/dashboardadmin/*" 
+                <Route
+                  path="/dashboardadmin/*"
                   element={
                     <ProtectedRoute>
                       <Dashboardadmin />
